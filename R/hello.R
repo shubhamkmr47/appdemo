5f0990c207287ee65366d71520310389c79fd4df
Shubham Kumar <shubhamkmr47@gmail.com>
2016-05-30 18:14:10 +0530

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -8,7 +8,5 @@ hello <- function(myname = ""){
   if(myname == ""){
     stop("Tell me your name!")
   }
-  list(
-    message = paste("hello", myname, "! This is", R.Version()$version.string)
-  )
+    paste("hello", myname, "! This is", R.Version()$version.string)
 }
